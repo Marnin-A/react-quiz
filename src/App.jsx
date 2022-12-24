bcdbfdc5b022cb2cf91619c25a177557233d2026
Marnin Audu <msmaudu@gmail.com>
2022-12-24 12:18:00 +0100

    Added random number generator to define index of each option

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import Card from "./components/card";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         Welcome to the React Quiz by{"\u00A0"}
         <a href="https://portfolio-marnin-a.vercel.app/">Marnin_a</a>
       </h1>
+      <Card />
     </div>
   );
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,7 +1,69 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./card.css";
 
 const card = () => {
-  return <div className="card">card</div>;
+  const [TriviaData, setTriviaData] = useState("");
+
+  // Get question and answer data
+  useEffect(() => {
+    axios
+      .get(
+        "https://opentdb.com/api.php?amount=1&category=18&difficulty=medium&type=multiple"
+      )
+      .then((res) => {
+        const TriviaData = res.data.results[0];
+        setTriviaData(TriviaData);
+        console.log(TriviaData);
+      })
+      .catch(() => {
+        console.log("There was an error");
+      });
+  }, []);
+
+  // Use function to generate random numbers
+  const arr = [];
+  function myRandomInts(quantity, max) {
+    while (arr.length < quantity) {
+      var candidateInt = Math.floor(Math.random() * max) + 1;
+      if (arr.indexOf(candidateInt) === -1) arr.push(candidateInt);
+    }
+    return arr;
+  }
+
+  // Call random numbers function and assign random indexes
+  // to each option
+  myRandomInts(4, 4);
+  let index1 = arr[0],
+    index2 = arr[1],
+    index3 = arr[2],
+    index4 = arr[3];
+
+  // Render Card
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h3>{TriviaData.question}</h3>
+        <div>
+          <input type="checkbox" name="" id="" />
+          <span>{index1}</span>
+        </div>
+        <div>
+          <input type="checkbox" name="" id="" />
+          {index2}
+        </div>
+        <div>
+          <input type="checkbox" name="" id="" />
+          {index3}
+        </div>
+        <div>
+          <input type="checkbox" name="" id="" />
+          {index4}
+        </div>
+      </div>
+      <button id="submit" type="submit"></button>
+    </div>
+  );
 };
 
 export default card;
