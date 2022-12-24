f3297c1ba78cd9e8704ff8cc5feab95bb0feec54
Marnin Audu <msmaudu@gmail.com>
2022-12-24 10:18:08 +0100

    Created card component, and page background colour

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Welcome to the React Quiz by{"\u00A0"}
+        <a href="https://portfolio-marnin-a.vercel.app/">Marnin_a</a>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/card.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const card = () => {
+  return <div className="card">card</div>;
+};
+
+export default card;
