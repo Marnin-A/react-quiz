4588e6ca461398b9beb07b981524dc7acfa61dee
Marnin Audu <msmaudu@gmail.com>
2022-12-27 11:08:23 +0100

    Added function to combine all options into one array, and display the options

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -5,13 +5,29 @@ import "./card.css";
 
 const card = () => {
   const [TriviaData, setTriviaData] = useState([]);
-  const [correctAnswer, setCorrectAnswer] = useState("");
-  const [incorrectAnswers, setinCorrectAnswers] = useState([]);
+  // const [correctAnswer, setCorrectAnswer] = useState("");
+  // const [incorrectAnswers, setinCorrectAnswers] = useState([]);
   const [allPossibleAnswers, setAllPossibleAnswers] = useState([]);
   const [Loading, setLoading] = useState(true);
 
+  //combines correct and incorrect answer into single array
+  async function combineAllAnswers(incorrectAnswers, correctAnswer) {
+    let allAnswers = [];
+
+    incorrectAnswers.map((incorrectAnswer) => {
+      allAnswers.push(incorrectAnswer);
+    });
+
+    allAnswers.push(correctAnswer);
+    //Randomize order of answers in array
+    allAnswers.sort(() => Math.random() - 0.5);
+    setAllPossibleAnswers(allAnswers);
+  }
+
+  let incorrectAnswers;
+  let correctAnswer;
   // Get question and answer data
-  useEffect(() => {
+  const NextQuestion = () => {
     axios
       .get(
         "https://opentdb.com/api.php?amount=1&category=18&difficulty=medium&type=multiple"
@@ -22,46 +38,19 @@ const card = () => {
         setTriviaData(TriviaData);
         const answer = TriviaData.correct_answer;
         const wrong_answers = TriviaData.incorrect_answers;
-        setinCorrectAnswers(wrong_answers);
-        setCorrectAnswer(answer);
-        console.log(incorrectAnswers);
+        incorrectAnswers = wrong_answers;
+        correctAnswer = answer;
+        combineAllAnswers(incorrectAnswers, correctAnswer);
+        console.log(correctAnswer);
       })
       .catch(() => {
         console.log("There was an error");
       });
+  };
+
+  useEffect(() => {
+    NextQuestion();
   }, []);
-
-  // Use function to generate random numbers
-  const arr = [];
-  function myRandomInts(quantity, max) {
-    while (arr.length < quantity) {
-      var candidateInt = Math.floor(Math.random() * max) + 1;
-      if (arr.indexOf(candidateInt) === -1) arr.push(candidateInt);
-    }
-    return arr;
-  }
-
-  // Call random numbers function and assign random indexes
-  // to each option
-  myRandomInts(4, 4);
-  let index1 = arr[0],
-    index2 = arr[1],
-    index3 = arr[2],
-    index4 = arr[3];
-
-  //combines correct and incorrect answer into single array
-  async function combineAllAnswers(incorrectAnswers, correctAnswer) {
-    let allAnswers = [];
-    incorrectAnswers.map((item) => {
-      item.incorrect_answers.map((incorrectAnswer) => {
-        allAnswers.push(incorrectAnswer);
-      });
-    });
-    allAnswers.push(correctAnswer);
-    //Randomize order of answers in array
-    allAnswers.sort(() => Math.random() - 0.5);
-    setAllPossibleAnswers(allAnswers);
-  }
 
   if (Loading) {
     return (
@@ -70,29 +59,43 @@ const card = () => {
       </div>
     );
   }
+  function removeCharacters(question) {
+    return question
+      .replace(/(&quot\;)/g, '"')
+      .replace(/(&rsquo\;)/g, '"')
+      .replace(/(&#039\;)/g, "'")
+      .replace(/(&amp\;)/g, '"');
+  }
   // Render Card
   return (
     <div className="card">
       <div className="card-content">
-        <h3>{TriviaData.question}</h3>
+        <h3>{removeCharacters(TriviaData.question)}</h3>
         <div>
           <input type="checkbox" name="" id="" />
-          <span>{index1}</span>
+          <span>{allPossibleAnswers[0]}</span>
         </div>
         <div>
           <input type="checkbox" name="" id="" />
-          {index2}
+          {allPossibleAnswers[1]}
         </div>
         <div>
           <input type="checkbox" name="" id="" />
-          {index3}
+          {allPossibleAnswers[2]}
         </div>
         <div>
           <input type="checkbox" name="" id="" />
-          {index4}
+          {allPossibleAnswers[3]}
         </div>
       </div>
-      <button id="submit" type="submit"></button>
+      <div className="btns">
+        <button id="submit" type="submit">
+          Submit
+        </button>
+        <button id="next" onClick={NextQuestion}>
+          Next
+        </button>
+      </div>
     </div>
   );
 };
