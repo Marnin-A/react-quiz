faffe1d7d27b07031b9e53b49f6012b70446142a
Marnin Audu <msmaudu@gmail.com>
2022-12-29 10:31:30 +0100

    Set category and difficulty values to be used in API url

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,24 @@ import "./App.css";
 import Quiz from "./components/card/quiz";
 
 function App() {
-  const [username, setUsername] = useState("Not Marnin");
+  const [username, setUsername] = useState("");
+  const [category, setCategory] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+
+  // Get the users preferred difficulty
+  const getDifficulty = (e) => {
+    console.log(e.target.value);
+    setDifficulty(e.target.value);
+  };
+
+  // Get the users preferred category
+  const getCategory = (e) => {
+    console.log(e.target.value);
+    setCategory(e.target.value);
+  };
+
+  // Define the URL
+  let URL = `https://opentdb.com/api.php?amount=1&category=${category}&difficulty=${difficulty}&type=multiple`;
   return (
     <div className="App">
       <h1>
@@ -12,41 +29,77 @@ function App() {
       </h1>
       <div className="card">
         <h3 className="app-h3">What name would you like to use?</h3>
-        <input className="name-input" type="text" />
+        {/* Get user's name to be used later on */}
+        <input
+          className="name-input"
+          type="text"
+          value={username}
+          onInput={(e) => {
+            setUsername(e.target.value);
+          }}
+        ></input>
+
+        {/* Create a div for the user to select their preferred difficulty and question category */}
         <div className="difficulties-categories">
           <div className="difficulties">
-            <h3 className="app-h3">Choose difficulty</h3>
-            <button className="difficulty-btn">Easy</button>
-            <button className="difficulty-btn">Meduim</button>
-            <button className="difficulty-btn">Hard</button>
+            <h3 value="" className="app-h3">
+              Choose difficulty
+            </h3>
+            <button
+              value="easy"
+              className="difficulty-btn"
+              onClick={getDifficulty}
+            >
+              Easy
+            </button>
+            <button
+              value="medium"
+              className="difficulty-btn"
+              onClick={getDifficulty}
+            >
+              Meduim
+            </button>
+            <button
+              value="hard"
+              className="difficulty-btn"
+              onClick={getDifficulty}
+            >
+              Hard
+            </button>
           </div>
           <div className="categories">
             <h3 className="app-h3">Choose category</h3>
-            <div>
-              <input type="checkbox" name="general-knowledge" id="" />
-              <label for="">General Knowledge</label>
-            </div>
-            <div>
-              <input type="checkbox" name="science-and-nature" id="" />
-              <label for="">Science and Nature</label>
-            </div>
-            <div>
-              <input type="checkbox" name="geography" id="" />
-              <label for="">Geography</label>
-            </div>
-            <div>
-              <input type="checkbox" name="mythology" id="" />
-              <label for="">Mythology</label>
-            </div>
-            <div>
-              <input type="checkbox" name="science-and-computers" id="" />
-              <label for="">Science and Computers</label>
-            </div>
+            <select
+              name=""
+              id="categories"
+              value={category}
+              onChange={getCategory}
+            >
+              <option value="questions" key="0">
+                Type of question
+              </option>
+              <option value="9">General Knowledge</option>
+
+              <option value="17">Science and Nature</option>
+
+              <option value="22">Geography</option>
+
+              <option value="20">Mythology</option>
+
+              <option value="18">Science and Computers</option>
+            </select>
           </div>
         </div>
-        <button id="start-quiz">Start Quiz</button>
+        <button
+          id="start-quiz"
+          onClick={() => {
+            console.log(username);
+          }}
+        >
+          Start Quiz
+        </button>
 
-        {/* <Quiz /> */}
+        {/* <Quiz username={username} /> */}
       </div>
     </div>
   );
