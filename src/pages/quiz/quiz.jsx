5cd5789d4ebf2c7107006ebbae215a6c6b818fa0
Marnin Audu <msmaudu@gmail.com>
2023-01-13 13:40:44 +0100

    Made layout responsive and added axios request function

--- src/pages/quiz/quiz.jsx
+++ src/pages/quiz/quiz.jsx
@@ -4,5 +4,48 @@ import styles from "./quiz.module.css";
 import axios from "axios";
 
 export const Quiz = () => {
-  return <div className={styles.quiz}>Man</div>;
+  const [TriviaData, setTriviaData] = useState([]);
+  const [Loading, setLoading] = useState(true);
+
+  // Retrieve the required data
+  let incorrectAnswers, correctAnser;
+  const getData = () => {
+    setLoading(false);
+    let URL = JSON.parse(localStorage.getItem("url"));
+    axios
+      .get(URL)
+      .then((res) => {
+        const Data = res.data.results[0];
+        incorrectAnswers = Data.incorrect_answers;
+        correctAnser = Data.correct_answer;
+        setTriviaData(Data);
+        // console.log(incorrectAnswers);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // Set loading animation
+  if (Loading) {
+    return (
+      <div>
+        <img
+          className={styles.LoadingAnimation}
+          src={LoadingAnimation}
+          alt="Loading"
+        />
+      </div>
+    );
+  }
+  return (
+    <div className={styles.quiz}>
+      {/* <div>{counter}/5</div> */}
+      <h3 className={styles.question}>{TriviaData.question}</h3>
+    </div>
+  );
 };
